f9f0a09f89758ab0ae2e8979d9320c2121e9b765
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-07-31 13:46:20 +0100

    made it not fixed

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./theme-toggle";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 z-20 w-full p-2 px-4 bg-white border-t border-gray-400 shadow flex items-center justify-between  dark:bg-gray-900 dark:border-gray-600">
+    <footer className="bottom-0 left-0 z-20 w-full p-2 px-4 bg-white border-t border-gray-400 shadow flex items-center justify-between  dark:bg-gray-900 dark:border-gray-600">
       <div className="text-emma-text font-extrabold ">
         Emgineers | {new Date().getFullYear()}
       </div>
