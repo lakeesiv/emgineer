a745f7e5e2127f4a7b2700b35fe55f88b1037a67
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-07-31 16:06:08 +0100

    progress

---
+++ app/blog/blog-post-card.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import Image from "next/image";
+import _mediaMap from "public/notion-media/media-map.json";
+import { mediaMapInterface } from "notion-on-next/types/types";
+import { BlogPageObjectResponse } from "types/notion-on-next.types";
+const mediaMap = _mediaMap as mediaMapInterface;
+
+export const BlogPostCard = ({
+  page,
+  databaseId,
+}: {
+  page: BlogPageObjectResponse;
+  databaseId: string;
+}) => {
+  const image = mediaMap[databaseId]?.[page.id]?.cover;
+
+  return (
+    <article
+      className={`max-w-md mx-auto md:max-w-none grid gap-6 md:gap-8 ${
+        image ? "md:grid-cols-2" : ""
+      }`}
+    >
+      {image && (
+        <a className="relative block group" href={`/blog/${page.slug}`}>
+          <Image
+            alt={page.title || "Cover Image for " + page.id}
+            src={mediaMap[databaseId]?.[page.id]?.cover}
+            className="max-h-[300px] w-full rounded-md  object-cover object-center"
+            width={300}
+            height={300}
+          />
+        </a>
+      )}
+      <div>
+        <header>
+          <h2 className="text-xl sm:text-2xl font-bold leading-snug mb-2 font-heading">
+            <a
+              className="text-emma-text underline underline-offset-4 decoration-1 transition ease-in duration-200"
+              href={`/blog/${page.slug}`}
+            >
+              {page.title}
+            </a>
+          </h2>
+        </header>
+        <p className="text-md sm:text-lg flex-grow">
+          {/* @ts-ignore -- Notion Team currently has incorrect type for RichTextObjectResponse. The API returns an an array of RichTextObjectResponse inside of RichTextPropertyItemObjectResponse, but the type definition is not aware of that yet  */}
+          {page.properties?.Description?.rich_text[0]?.plain_text}
+        </p>
+        <footer className="mt-4">
+          <div>
+            <span className="text-gray-500 dark:text-slate-400">
+              {formatDate(page.properties.Date.date?.start)}
+            </span>
+          </div>
+        </footer>
+      </div>
+    </article>
+  );
+};
+
+const formatDate = (date: string | undefined) => {
+  const d = new Date(date as string) || new Date();
+  // format date as Mon, Day Year (e.g. Nov 4, 2020)
+  return d.toLocaleDateString(undefined, {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+};
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import { BlogPageObjectResponse } from "types/notion-on-next.types";
 import { cachedGetParsedPages } from "../get";
-import { ProgrammingPageCard } from "./ProgrammingPageCard";
+import { BlogPostCard } from "./blog-post-card";
 
 export const revalidate = 60;
 
@@ -13,16 +13,12 @@ export default async function ProgrammingBlog() {
   return (
     <div>
       <main className="">
-        <h1 className="text-center font-bold text-3xl m-6 md:m-12">
+        <h1 className="text-center font-extrabold text-emma-text text-3xl m-6 md:m-6">
           Blog Posts
         </h1>
-        <div className="flex flex-col justify-center gap-8 max-w-[800px] mx-auto p-12">
+        <div className="space-y-8 gap-8 max-w-[800px] mx-auto p-12">
           {sortedPages.map((page) => (
-            <ProgrammingPageCard
-              page={page}
-              databaseId={databaseId}
-              key={page.id}
-            />
+            <BlogPostCard page={page} databaseId={databaseId} key={page.id} />
           ))}
         </div>
       </main>
