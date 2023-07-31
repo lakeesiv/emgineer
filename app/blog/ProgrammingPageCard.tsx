31c4ae057fd4db1b5d52ce55a5b026aa36fffaf3
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-07-31 11:07:25 +0100

    updates

--- app/blog/ProgrammingPageCard.tsx
+++ app/blog/ProgrammingPageCard.tsx
@@ -30,25 +30,11 @@ export const ProgrammingPageCard = ({
         <div className="p-4 flex flex-col gap-4 md:w-1/2">
           <div className="text-lg lg:text-2xl font-semibold">{page.title}</div>
           <div className="text-gray-400">
-            {new Date(page.created_time).toLocaleDateString()}
+            {page.properties.Date.date?.start}
           </div>
           <div className="">
             {/* @ts-ignore -- Notion Team currently has incorrect type for RichTextObjectResponse. The API returns an an array of RichTextObjectResponse inside of RichTextPropertyItemObjectResponse, but the type definition is not aware of that yet  */}
             {page.properties?.Description?.rich_text[0]?.plain_text}
-          </div>
-          <div className="">
-            {page.properties.Tags.multi_select.map((tag, index) => {
-              return (
-                <span
-                  className={`mr-2 px-2 py-1 rounded-lg text-white`}
-                  // can't use template literal to pull tag.color into a tailwind class, so we have to use the style prop.
-                  style={{ backgroundColor: tag.color }}
-                  key={tag.name + index}
-                >
-                  {tag.name}{" "}
-                </span>
-              );
-            })}
           </div>
         </div>
       </div>
