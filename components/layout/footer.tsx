96b265f8c6e301b5c62257814fcf396e1d2be1b0
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-01 12:13:41 +0100

    changed up colors

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./theme-toggle";
 
 const Footer = () => {
   return (
-    <footer className="w-full p-2 px-4 bg-white border-t border-gray-300 shadow flex items-center justify-between  dark:bg-gray-900 dark:border-gray-600">
+    <footer className="w-full p-2 px-4 bg-white border-t border-gray-300 shadow flex items-center justify-between  dark:bg-gray-800 dark:border-gray-600">
       <div className="text-emma-text font-extrabold ">
         Emgineers | {new Date().getFullYear()}
       </div>
