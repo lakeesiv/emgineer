ae7fd6d7a8a41d59b5e77dc36a25cbdfec6ede85
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-01 16:03:03 +0100

    updated blog section page

--- app/(home)/blog-section.tsx
+++ app/(home)/blog-section.tsx
@@ -6,8 +6,10 @@ const BlogSection = async () => {
   const pages = await getBlogPages(4);
 
   return (
-    <section className="p-8">
-      <h2 className="text-3xl font-extrabold text-emma-text">Our Blog</h2>
+    <section className="p-8 px-24">
+      <h2 className="text-3xl font-extrabold text-emma-text sm:text-center md:text-left">
+        Our Blog
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
         {pages.map((page) => (
           <BlogCard key={page.id} page={page} />
