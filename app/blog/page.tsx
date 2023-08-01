e052fab38a2ccd0607e043fd3cc2d51d007f3982
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-01 11:40:47 +0100

    updated blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,7 +12,7 @@ export default async function ProgrammingBlog() {
 
   return (
     <div>
-      <h1 className="text-center font-extrabold text-emma-text text-3xl m-6 md:m-6">
+      <h1 className="text-center font-extrabold text-emma-text text-3xl m-4">
         Blog Posts
       </h1>
       <div className="space-y-8 gap-8 max-w-[800px] mx-auto p-12">
