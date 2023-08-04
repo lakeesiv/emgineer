4dd533f1e368ca2b66b0e6f10e359bad4dfd84a0
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-04 19:05:24 +0100

    head

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <title>Emgineers</title>
+        <meta
+          name="description"
+          content="Emmanuel College Engineering Society"
+        />
+        <meta property="og:title" content="Emgineers" />
+        <meta
+          property="og:description"
+          content="Emmanuel College Engineering Society"
+        />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta property="og:image" content="https://emgineer.vercel.app/og/" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         <Providers>
           <div className="flex flex-col min-h-screen">
