c4a1a2726d443c9bf12fe4b6d6935f774baa732c
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-04 14:23:36 +0100

    OG Image

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,14 @@ export default function Head() {
   return (
     <>
       <title>Emgineers</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Emmanuel College Engineering Society" />
+      <meta property="og:title" content="Emgineers" />
+      <meta
+        property="og:description"
+        content="Emmanuel College Engineering Society"
+      />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta property="og:image" content="https://emgineer.vercel.app/og/" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
