28de31d5eb0a5dbfbdb9224f3188c47b5068513b
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-08 13:52:12 +0100

    updated 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { AnimatedTitle } from "components/text";
 import { Card } from "components/ui/card";
 import Image from "next/image";
@@ -15,32 +16,32 @@ const NotFoud: FC = ({}) => {
         height={500}
         className="mt-4 rounded-md  object-cover object-center"
       />
-      <div className="grid grid-cols-3 gap-4 w-[500px] mt-4">
-        <Card className="col-span-2 p-4 flex  align-center items-center text-center">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 max-w-[500px] mt-4 text-sm md:text-md">
+        <Card className="md:col-span-2 p-4 flex  align-center items-center text-center">
           <p className="font-extrabold text-emma-primary">What will you do?</p>
         </Card>
         <Card className="grid grid-cols-2 grid-rows-2 p-4 gap-4">
           <Link
             href="/"
-            className="text-emma-primary hover:text-emma-secondary font-extrabold"
+            className="transition-colors text-emma-primary hover:text-emma-secondary font-extrabold underline underline-offset-4"
           >
             Home
           </Link>
           <Link
             href="/blog"
-            className="text-emma-primary hover:text-emma-secondary font-extrabold"
+            className="transition-colors text-emma-primary hover:text-emma-secondary font-extrabold underline underline-offset-4"
           >
             Blog
           </Link>
           <Link
             href="/events"
-            className="text-emma-primary hover:text-emma-secondary font-extrabold"
+            className="transition-colors text-emma-primary hover:text-emma-secondary font-extrabold underline underline-offset-4"
           >
             Events
           </Link>
           <Link
             href="/credits"
-            className="text-emma-primary hover:text-emma-secondary font-extrabold"
+            className="transition-colors text-emma-primary hover:text-emma-secondary font-extrabold underline underline-offset-4"
           >
             Credits
           </Link>
