229548e458db2b22460412a1372ef571717cfddb
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-11 11:13:33 +0100

    switched back to node.js runtime

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -2,7 +2,7 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "server/api/root";
 import { createTRPCContext } from "server/api/trpc";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 const handler = (req: Request) =>
   fetchRequestHandler({
