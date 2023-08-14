07a1a27cd368d32ca05a46d4f823ecf5187fbb22
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-14 15:31:27 +0100

    toast

--- components/layout/providers.tsx
+++ components/layout/providers.tsx
@@ -2,11 +2,13 @@
 
 import { ThemeProvider } from "next-themes";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "components/ui/toaster";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" themes={["light", "dark"]}>
       <SessionProvider>{children}</SessionProvider>
+      <Toaster />
     </ThemeProvider>
   );
 }
