85e9d2424f97f5f0f22a0e42fbc2fb6411d25dac
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-17 14:41:24 +0100

    apply for funding

--- app/funding/page.tsx
+++ app/funding/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TimeLine from "./timeline";
 import { Title } from "components/text";
+import siteConfig from "site.config";
 
 const Page = () => {
   return (
@@ -12,6 +13,20 @@ const Page = () => {
       </p>
 
       <TimeLine className="mt-8 md:mt-12" />
+
+      <div className="grid gap-8 items-start justify-center mt-12">
+        <div className="relative group">
+          <div className="absolute -inset-0.5 bg-gradient-to-tr from-emma-blue  via-emma-pink-secondary   to-emma-blue rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 "></div>
+          <a
+            className="relative px-7 py-4 bg-gray-50 dark:bg-black rounded-lg leading-none flex items-center "
+            href={siteConfig.fundingUrl}
+          >
+            <span className="font-bold text-emma-primary group-hover:text-emma-secondary transition duration-1000">
+              Apply for Funding
+            </span>
+          </a>
+        </div>
+      </div>
     </main>
   );
 };
--- site.config.ts
+++ site.config.ts
@@ -5,6 +5,7 @@ const siteConfig = {
   blogDatabaseId: "a83071d8-1416-44a9-98cf-45638a583a82",
   eventsDatabaseId: "533a35ca-b69f-4a5d-add8-35c083eaa97e",
   signUpDatabaseId: "68bdc66c-47c7-4ec3-8353-895c3fce0ecc",
+  fundingUrl: "https://youtu.be/dQw4w9WgXcQ",
   commmittee: committee as CommitteeMember[],
   primarySiteUrl: "https://emgineer.vercel.app",
 };
