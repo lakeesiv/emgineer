d158ffccc84e30c05056a7d5d07498ab0f5f591b
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-17 21:15:31 +0100

    test vercel

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,7 +1,11 @@
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
+// import { neon, neonConfig } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
 
-neonConfig.fetchConnectionCache = true;
-const sql = neon(process.env.DRIZZLE_DB_URL!);
+// neonConfig.fetchConnectionCache = true;
+// const sql = neon(process.env.DRIZZLE_DB_URL!);
+
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+
 export const db = drizzle(sql, { schema });
