39e535129fb40de47edf6768cad4941429bc678a
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-08-23 18:15:09 +0000

    revert; issues with 0.28.3

--- app/(misc)/admin/table/columns.tsx
+++ app/(misc)/admin/table/columns.tsx
@@ -4,4 +4,13 @@ import { ColumnDef } from "@tanstack/react-table";
 import { eventSignUps } from "lib/db/schema";
 import { type InferSelectModel } from "drizzle-orm";
 
-type SignUp = InferSelectModel<typeof eventSignUps>;
+type SignUp = typeof eventSignUps.$inferSelect
+
+export const columns: ColumnDef<SignUp>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+	accessorKey: "",
+];
