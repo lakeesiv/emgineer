c506d67cabe4e79026a8f4628e6fd6cacab71a88
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-08-23 17:26:46 +0000

    disabling behaviour

--- app/(misc)/admin/_components/revalidate-button.tsx
+++ app/(misc)/admin/_components/revalidate-button.tsx
@@ -3,22 +3,33 @@ import { FC } from "react";
 import { api } from "trpc/client";
 import { useToast } from "components/ui/use-toast";
 import { Button } from "components/ui/button";
+import { useState } from "react";
 
 interface RevalidateButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const RevalidateButton: FC<RevalidateButtonProps> = ({ ...props }) => {
   const { toast } = useToast();
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <Button
       {...props}
+      disabled={disabled}
       onClick={async () => {
         try {
+          setDisabled(
+            (s) => true // disable button
+          );
           await api.web.revalidate.query();
           toast({
             title: "Synced with Notion",
             description: "The website has been synced with Notion",
+            duration: 1000,
           });
+          setDisabled(
+            (s) => false // reset disabled
+          );
         } catch (error) {
           toast({
             title: "Error",
