198a534192681636da709f18668e3ba411b2288f
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-09-22 16:33:03 +0000

    random start effect

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import BlogSection, { BlogSectionLoading } from "./_components/blog-section";
 import EventSection, { EventSectionLoading } from "./_components/event-section";
 import { Description, Title } from "components/text";
+import AnimatedCanvas from "./_components/animated-canvas";
 
 export const runtime = "nodejs";
 export const revalidate = 86400;
@@ -9,9 +10,11 @@ export const revalidate = 86400;
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col items-center justify-center p-12">
-        <Title animate>Emgineers</Title>
-        <Description>Emmanuel College Engineering Society</Description>
+      <section className="flex flex-col items-center justify-center">
+        <AnimatedCanvas>
+          <Title animate>Emgineers</Title>
+          <Description>Emmanuel College Engineering Society</Description>
+        </AnimatedCanvas>
       </section>
       <Suspense fallback={<EventSectionLoading />}>
         <EventSection />
