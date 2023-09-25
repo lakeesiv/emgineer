fcef8bb0c4250c84839bbf30354a22711c3c4308
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-09-25 14:30:59 +0000

    fixed padding

--- app/events/_components/event-card.tsx
+++ app/events/_components/event-card.tsx
@@ -45,11 +45,7 @@ export const EventCard = ({
   const slug = page.parsed.eventId;
 
   return (
-    <article
-      className={`max-w-md mx-auto md:max-w-none grid gap-6 md:gap-8 ${
-        image ? "md:grid-cols-2" : ""
-      }`}
-    >
+    <article className={`grid gap-6 md:gap-8 ${image ? "md:grid-cols-2" : ""}`}>
       {image && (
         <Link className="relative block group" href={`/events/${slug}`}>
           <Image
@@ -94,37 +90,3 @@ export const EventCard = ({
     </article>
   );
 };
-
-// const LocationAndTime = ({
-//   page,
-//   children,
-// }: {
-//   page: ParsedEventsPageObjectResponse;
-//   children?: React.ReactNode;
-// }) => {
-//   // only renedr on client
-//   if (typeof window === "undefined") return null;
-
-//   return (
-//     <span className="text-gray-500 dark:text-slate-400">
-//       {formatDate(page.parsed.date)} @ {page.parsed.location}
-//     </span>
-//   );
-// };
-
-// export const formatDate = (date: Date) => {
-//   const days = date.toLocaleDateString("en-GB", {
-//     month: "short",
-//     day: "numeric",
-//     year: "numeric",
-//   });
-
-//   // time: HH:MM AM/PM
-//   const time = date.toLocaleTimeString("en-GB", {
-//     hour: "numeric",
-//     minute: "numeric",
-//     hour12: true,
-//   });
-
-//   return `${time}, ${days}`;
-// };
